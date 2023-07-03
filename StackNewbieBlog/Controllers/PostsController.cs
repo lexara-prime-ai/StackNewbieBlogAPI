77e0a2f55743f4030fc4e5b1ea16a361f005cf4c
irfanghat <irfanghat@gmail.com>
2023-07-03 14:49:52 +0300

    UPDATE: Added update and delete routes and controllers

--- StackNewbieBlog/Controllers/PostsController.cs
+++ StackNewbieBlog/Controllers/PostsController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using StackNewbieBlog.Data;
+using StackNewbieBlog.Models.DTO;
+using StackNewbieBlog.Models.Entities;
 
 namespace StackNewbieBlog.Controllers
 {
@@ -31,6 +33,7 @@ namespace StackNewbieBlog.Controllers
 
         // Get single Post
         [HttpGet]
+        [ActionName("GetPostById")]
         [Route("{id:guid}")]
         public async Task<IActionResult> GetPostById(Guid id)
         {
@@ -42,5 +45,93 @@ namespace StackNewbieBlog.Controllers
             }
             return NotFound();
         }
+
+        // Add Post
+        [HttpPost]
+        public async Task<IActionResult> AddPost(AddPostRequest addPostRequest)
+        {
+            // Convert DTO(Data Transfer Object) to Entity
+            var post = new Post()
+            {
+                Title = addPostRequest.Title,
+                Content = addPostRequest.Content,
+                Author = addPostRequest.Author,
+                FeaturedImageUrl = addPostRequest.FeaturedImageUrl,
+                PublishDate = addPostRequest.PublishDate,
+                UpdatedDate = addPostRequest.UpdatedDate,
+                Summary = addPostRequest.Summary,
+                UrlHandle = addPostRequest.UrlHandle,
+                Visible = addPostRequest.Visible
+            };
+
+            // Create and assign an Id
+            post.Id = Guid.NewGuid();
+            await dbContext.Posts.AddAsync(post);
+            // Save changes to the SQL database
+            await dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPostById), new { id = post.Id }, post);
+        }
+
+        // Update Post
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdatePost([FromRoute] Guid id, UpdatePostRequest updatePostRequest)
+        {
+            // Convert DTO(Data Transfer Object) to Entity
+            var post = new Post()
+            {
+                Title = updatePostRequest.Title,
+                Content = updatePostRequest.Content,
+                Author = updatePostRequest.Author,
+                FeaturedImageUrl = updatePostRequest.FeaturedImageUrl,
+                PublishDate = updatePostRequest.PublishDate,
+                UpdatedDate = updatePostRequest.UpdatedDate,
+                Summary = updatePostRequest.Summary,
+                UrlHandle = updatePostRequest.UrlHandle,
+                Visible = updatePostRequest.Visible
+            };
+
+            // Check if post exists
+            var existingPost = await dbContext.Posts.FindAsync(id);
+
+            if (existingPost != null)
+            {
+                existingPost.Title = updatePostRequest.Title;
+                existingPost.Content = updatePostRequest.Content;
+                existingPost.Author = updatePostRequest.Author;
+                existingPost.FeaturedImageUrl = updatePostRequest.FeaturedImageUrl;
+                existingPost.PublishDate = updatePostRequest.PublishDate;
+                existingPost.UpdatedDate = updatePostRequest.UpdatedDate;
+                existingPost.Summary = updatePostRequest.Summary;
+                existingPost.UrlHandle = updatePostRequest.UrlHandle;
+                existingPost.Visible = updatePostRequest.Visible;
+
+                await dbContext.SaveChangesAsync();
+
+                return Ok(existingPost);
+            }
+            return NotFound();
+        }
+
+        // Delete Post
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeletePost(Guid id)
+        {
+            var existingPost = await dbContext.Posts.FindAsync(id);
+
+            if (existingPost != null)
+            {
+                // Pass in the existingPost entity as an argument
+                dbContext.Remove(existingPost);
+                await dbContext.SaveChangesAsync();
+
+                return Ok(existingPost);
+            }
+
+            return NotFound();
+        }
+
     }
 }
