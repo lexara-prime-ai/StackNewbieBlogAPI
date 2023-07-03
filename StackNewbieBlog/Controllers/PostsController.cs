c1234aaf61ef83247cbde467642e8f64c1fc0acc
irfanghat <irfanghat@gmail.com>
2023-07-03 13:40:36 +0300

    UPDATE: Added get post by id and add post routes and controllers

--- StackNewbieBlog/Controllers/PostsController.cs
+++ StackNewbieBlog/Controllers/PostsController.cs
@@ -21,11 +21,26 @@ namespace StackNewbieBlog.Controllers
             this.dbContext = dbContext;
         }
 
+        // Get all Posts
         [HttpGet]
         public async Task<IActionResult> GetAllPosts()
         {
             var posts = await dbContext.Posts.ToListAsync();
             return Ok(posts);
+        }
+
+        // Get single Post
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetPostById(Guid id)
+        {
+            var post = await dbContext.Posts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (post != null)
+            {
+                return Ok(post);
+            }
+            return NotFound();
         }
     }
 }
---
+++ StackNewbieBlog/Models/DTO/AddPostRequest.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackNewbieBlog.Models.DTO
+{
+    public class AddPostRequest
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Summary { get; set; }
+
+        public string UrlHandle { get; set; }
+
+        public string FeaturedImageUrl { get; set; }
+
+        public bool Visible { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+    }
+}
